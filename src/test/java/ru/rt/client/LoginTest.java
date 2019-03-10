95128df9562b9514318e88f6110c703b957ab8ae
anv0l <andrei.voloshin@live.com>
2019-03-10 16:14:50 +0300

    1. Первая итерация рефакторинга. 2. Добавлен класс GroupData.

--- src/test/java/ru/rt/client/LoginTest.java
+++ src/test/java/ru/rt/client/LoginTest.java
@@ -1,18 +1,24 @@
 package ru.rt.client;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
-
-import static org.testng.Assert.*;
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
 
 public class LoginTest {
+    public static final String LOGIN_USER_FIELD = "user";       // element name for 'user name' field login page
+    public static final String LOGIN_PASS_FIELD = "pass";       // element name for 'user pass' field for login page
+    public static final String GROUPS = "groups";               // element *link* for 'groups'
+    public static final String GROUPS_NEW = "new";              // element name for 'add new group'
+    public static final String GROUP_NEW_NAME = "group_name";   // element name for 'new group name'
+    public static final String GROUP_NEW_HEADER = "group_header"; // element name for 'new group header'
+    public static final String GROUP_NEW_FOOTER = "group_footer"; // element name for 'new group footer'
+    public static final String GROUP_NEW_SUBMIT = "submit";     // element name for 'new group submit'
     private WebDriver driver;
     private String baseUrl;
     private boolean acceptNextAlert = true;
@@ -24,28 +30,50 @@ public class LoginTest {
         baseUrl = "http://localhost/addressbook/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get(baseUrl);
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
+        login("admin", "secret");
+    }
+
+    private void login(String login, String password) {
+        driver.findElement(By.name(LOGIN_USER_FIELD)).click();
+        driver.findElement(By.name(LOGIN_USER_FIELD)).clear();
+        driver.findElement(By.name(LOGIN_USER_FIELD)).sendKeys(login);
+        driver.findElement(By.name(LOGIN_PASS_FIELD)).click();
+        driver.findElement(By.name(LOGIN_PASS_FIELD)).clear();
+        driver.findElement(By.name(LOGIN_PASS_FIELD)).sendKeys(password);
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
     }
 
 
     @Test
     public void testAddGroup() throws Exception {
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("test");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("header test");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("footer test");
-        driver.findElement(By.name("submit")).click();
+        navToGroupPage();
+        initAddGroup();
+        fillGroupForm(new GroupData("Group name", "header test", "footer test"));
+        submitAddGroup();
+    }
+
+    private void submitAddGroup() {
+        driver.findElement(By.name(GROUP_NEW_SUBMIT)).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        driver.findElement(By.name(GROUP_NEW_NAME)).click();
+        driver.findElement(By.name(GROUP_NEW_NAME)).clear();
+        driver.findElement(By.name(GROUP_NEW_NAME)).sendKeys(groupData.getName());
+        driver.findElement(By.name(GROUP_NEW_HEADER)).click();
+        driver.findElement(By.name(GROUP_NEW_HEADER)).clear();
+        driver.findElement(By.name(GROUP_NEW_HEADER)).sendKeys(groupData.getHeader());
+        driver.findElement(By.name(GROUP_NEW_FOOTER)).click();
+        driver.findElement(By.name(GROUP_NEW_FOOTER)).clear();
+        driver.findElement(By.name(GROUP_NEW_FOOTER)).sendKeys(groupData.getFooter());
+    }
+
+    private void initAddGroup() {
+        driver.findElement(By.name(GROUPS_NEW)).click();
+    }
+
+    private void navToGroupPage() {
+        driver.findElement(By.linkText(GROUPS)).click();
     }
 
     @AfterClass(alwaysRun = true)
