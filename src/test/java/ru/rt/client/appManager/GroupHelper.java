1e943ceb688c53ff7e3a4f51d785eefb2624086c
anv0l <andrei.voloshin@live.com>
2019-04-21 17:32:54 +0300

    Дальнейший рефакторинг: 1. Создание базового класса-помощника, в который вынесены общие для классов-помощников методы - ввод теста и нажатие кнопки.

--- src/test/java/ru/rt/client/appManager/GroupHelper.java
+++ src/test/java/ru/rt/client/appManager/GroupHelper.java
@@ -4,45 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.rt.client.model.GroupData;
 
-public class GroupHelper {
+public class GroupHelper extends HelperBase {
     private static final String GROUP_NEW_SUBMIT = "submit";     // element name for 'new group submit'
     private static final String GROUPS_NEW = "new";              // element name for 'add new group'
     private static final String GROUP_NEW_NAME = "group_name";   // element name for 'new group name'
-    private static final String GROUP_NEW_HEADER = "group_header"; // element name for 'new group header'
     private static final String GROUP_NEW_FOOTER = "group_footer"; // element name for 'new group footer'
     private static final String GROUPS_SELECTED_FIRST = "(//span[@class='group']/input[@name='selected[]'])[1]";// element xpath for 'selected group', will always select first element
     private static final String GROUPS_DELETE = "delete";        // element name for 'delete selected group'
-    private WebDriver driver;
 
     public GroupHelper(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name(GROUP_NEW_NAME)).click();
-        driver.findElement(By.name(GROUP_NEW_NAME)).clear();
-        driver.findElement(By.name(GROUP_NEW_NAME)).sendKeys(groupData.getName());
-        driver.findElement(By.name(GROUP_NEW_HEADER)).click();
-        driver.findElement(By.name(GROUP_NEW_HEADER)).clear();
-        driver.findElement(By.name(GROUP_NEW_HEADER)).sendKeys(groupData.getHeader());
-        driver.findElement(By.name(GROUP_NEW_FOOTER)).click();
-        driver.findElement(By.name(GROUP_NEW_FOOTER)).clear();
-        driver.findElement(By.name(GROUP_NEW_FOOTER)).sendKeys(groupData.getFooter());
+        type(By.name(GROUP_NEW_NAME), groupData.getName());
+        type(By.name(GROUP_NEW_HEADER), groupData.getHeader());
+        type(By.name(GROUP_NEW_FOOTER), groupData.getFooter());
     }
 
     public void initAddGroup() {
-        driver.findElement(By.name(GROUPS_NEW)).click();
+        click(By.name(GROUPS_NEW));
     }
 
     public void deleteSelectedGroup() {
-        driver.findElement(By.name(GROUPS_DELETE)).click();
+        click(By.name(GROUPS_DELETE));
     }
 
     public void selectGroup() {
-        driver.findElement(By.xpath(GROUPS_SELECTED_FIRST)).click();
+        click(By.xpath(GROUPS_SELECTED_FIRST));
     }
 
     public void submitAddGroup() {
-        driver.findElement(By.name(GROUP_NEW_SUBMIT)).click();
+        click(By.name(GROUP_NEW_SUBMIT));
     }
 }
---
+++ src/test/java/ru/rt/client/appManager/HelperBase.java
@@ -0,0 +1,59 @@
+package ru.rt.client.appManager;
+
+import org.openqa.selenium.*;
+
+public class HelperBase {
+    protected static final String GROUP_NEW_HEADER = "group_header"; // element name for 'new group header'
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    protected WebDriver driver;
+
+    public HelperBase(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
